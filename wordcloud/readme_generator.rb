f9515262473b564103731e3cba3d024e93d5abba
Prachi Arya <109187571+Prachiofficial@users.noreply.github.com>
2022-07-20 00:05:49 +0530

    done!!

--- wordcloud/readme_generator.rb
+++ wordcloud/readme_generator.rb
@@ -30,26 +30,25 @@ class ReadmeGenerator
     end
 
     markdown = <<~HTML
-    <img src="https://github.com/HarshitKumar9030/HarshitKumar9030/blob/main/github.png">
+<img src="https://github.com/HarshitKumar9030/HarshitKumar9030/blob/main/github.png">
 
-    # Hi there I am [Prachi 🖐️][website]
-    <br />
+# Hi there I am [Prachi 🖐️][website]
+<br />
     
-    ## I am a developer and a student.
+## I am a developer and a student.
     
-    - 👋 Hi, I’m Prachi Arya
-    - 👀 I’m interested in Playing Guitar, Linux and Javascript ...
-    - 🌱 I’m currently learning React .
-    - 📫 Mail me at Prachi@prachiarya.me
-    
-    <br />
+- 👋 Hi, I’m Prachi Arya
+- 👀 I’m interested in Playing Guitar, Linux and Javascript ...
+- 🌱 I’m currently learning React .
+- 📫 Mail me at Prachi@prachiarya.me
+<br />
 
-    ## Github Info / Stats
-  <div>
-    <a align="left" href="#"><img alt="Prachi's Github Stats" src="https://github-readme-stats.vercel.app/api?username=Prachiofficial&show_icons=true&include_all_commits=true&count_private=true&theme=react&hide_border=true&bg_color=0D1117&title_color=5ce1e6&icon_color=5ce1e6" height="200"/></a>
-    <a align="right" href="#"><img alt="Prachiofficial's Top Languages" src="https://github-readme-stats.vercel.app/api/top-langs/?username=Prachiofficial&langs_count=10&layout=compact&theme=react&hide_border=true&bg_color=0D1117&title_color=5ce1e6&icon_color=5ce1e6" height="200"/></a>
-   <p align="center"> <img src="https://komarev.com/ghpvc/?username=Prachiofficial-1nc0re&label=Profile%20views&color=0e75b6&style=flat" alt="Prachiofficial" /> </p>
-    <br/>
+## Github Info / Stats
+<div>
+<a align="left" href="#"><img alt="Prachi's Github Stats" src="https://github-readme-stats.vercel.app/api?username=Prachiofficial&show_icons=true&include_all_commits=true&count_private=true&theme=react&hide_border=true&bg_color=0D1117&title_color=5ce1e6&icon_color=5ce1e6" height="200"/></a>
+<a align="right" href="#"><img alt="Prachiofficial's Top Languages" src="https://github-readme-stats.vercel.app/api/top-langs/?username=Prachiofficial&langs_count=10&layout=compact&theme=react&hide_border=true&bg_color=0D1117&title_color=5ce1e6&icon_color=5ce1e6" height="200"/></a>
+<p align="center"> <img src="https://komarev.com/ghpvc/?username=Prachiofficial-1nc0re&label=Profile%20views&color=0e75b6&style=flat" alt="Prachiofficial" /> </p>
+  <br/>
     <i><b>Note:</b> Top languages is only a metric of the languages my public code consists of and doesn't reflect experience or skill level.</i>
   </div>
 
